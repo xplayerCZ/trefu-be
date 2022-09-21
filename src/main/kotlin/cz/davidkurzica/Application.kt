da90fba6dbab0ae1b7397826e560f631b1f33eea
xplayerCZ <davidkurzica18@gmail.com>
2022-09-21 17:37:04 +0200

    feat: updated Application.kt

--- src/main/kotlin/cz/davidkurzica/Application.kt
+++ src/main/kotlin/cz/davidkurzica/Application.kt
@@ -1,58 +1,22 @@
 package cz.davidkurzica
 
 import cz.davidkurzica.db.DatabaseFactory
-import cz.davidkurzica.di.serviceModule
-import cz.davidkurzica.web.*
-import io.ktor.serialization.kotlinx.json.*
+import cz.davidkurzica.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.callloging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.resources.*
-import io.ktor.server.routing.*
-import kotlinx.serialization.json.Json
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
-import org.slf4j.event.Level
+
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
+@Suppress("unused") // application.conf references the main function.
 fun Application.module() {
-    install(CallLogging) {
-        level = Level.INFO
-        format { call ->
-            val request = call.request
-            val response = call.response
-            "[${response.status()}] ${request.httpMethod.value} ${request.uri}"
-
-        }
-    }
-    install(Resources)
-    install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            isLenient = true
-        })
-    }
-
-    install(Koin) {
-        slf4jLogger()
-        modules(serviceModule)
-    }
+    configureResources()
+    configureHTTP()
+    configureRouting()
+    configureSerialization()
+    configureKoin()
+    configureMonitoring()
 
     DatabaseFactory.initDatabase(this.environment.config)
-
-    routing {
-        connection()
-        connectionRule()
-        line()
-        packet()
-        stop()
-        departure()
-        route()
-        routeStop()
-        rule()
-    }
 }
 
